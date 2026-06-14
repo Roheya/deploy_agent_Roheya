ee91715a1315399773b022ce27f8d34a51bb64fa
root <r.touray@alustudent.com>
2026-06-14 00:01:45 -0700

    updating the attendance thresholds

--- setup_project.sh
+++ setup_project.sh
@@ -19,3 +19,23 @@ vi config.json
 cd -
 cd reports
 vi reports.log
+
+#2. Dynamic Configuration (Stream Editing)
+# deciding if I want  to update the attendance thresholds
+echo "Do I want to update the attendance thresholds? (y/n)"
+read update_choice
+if [ $update_choice = y ]; then
+        echo "new warning threshold (default 75):"
+        read warning
+        warning=${warning:-75}
+        echo "Enter new Failure threshold (default 50):"
+        read failure
+        failure=${failure:-50}
+
+# using the sed command to perform an "in-place" edit of the config.json file
+sed -i "s/\"warning\": 75/\"warning\": $warning/" /root/deploy_agent_Roheya/attendance_tracker_v1/Helpers/config.json
+sed -i "s/\"failure\": 50/\"failure\": $failure/" /root/deploy_agent_Roheya/attendance_tracker_v1/Helpers/config.json
+else
+    echo "no changes"
+fi
+
