6a388b5c8d5790791fcd1a7a67e229ea546168f5
root <r.touray@alustudent.com>
2026-06-15 04:12:01 -0700

    checking if python3 exits and also deleting the incomplete directory

--- setup_project.sh
+++ setup_project.sh
@@ -54,4 +54,40 @@ else
     echo "no changes"
 fi
 
+# 4. Environment Validation
+# Verify python3 installation
+version=$(python3 --version 2>/dev/null)
+if [ -n "$version" ]; then
+        echo "Success: Python3 is installed ($version)"
+else
+        echo "Warning: Python3 is not installed."
+fi
+# Verify application directory structure
+project_directory="/root/deploy_agent_Roheya/attendance_tracker_v1"
+if [ -d "$project_directory" ];then
+        echo "Success: Application directory exists at $project_directory"
+else
+        echo "Warning: Application directory missing: $project_directory"
+fi
+
+# 3. Process Management (The Trap)
+project_directory="/root/deploy_agent_Roheya/attendance_tracker_v1"
+archive_name="attendance_tracker_v1_archive"
+
+# Trap SIGINT (Ctrl+C) and call the handler
+trap 'handle_user_interrupt' SIGINT
+
+# creating attendance_tracker_v1_archive and removing the incomplete directory
+handle_user_interrupt() {
+        echo "Interrupt detected!"
+        tar -cf "$archive_name" "$project_directory"
+        echo "Archive created: $archive_name"
+        rm -rf "$project_directory"
+        echo "Incomplete project directory removed"
+        echo "Cleanup complete. Exiting"
+        exit
+}
+
+sleep 30
+                                                                                                       
 
