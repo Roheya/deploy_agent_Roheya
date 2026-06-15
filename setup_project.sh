da096ac51a1e24244ef9b9d5efba3be0fe91441c
root <r.touray@alustudent.com>
2026-06-15 06:45:46 -0700

    check if the project directory already exits

--- setup_project.sh
+++ setup_project.sh
@@ -18,9 +18,15 @@ handle_user_interrupt() {
 }
 
 sleep 10
+
 # 1. Directory Architecture
-mkdir -p attendance_tracker_v1
-cd attendance_tracker_v1
+if [ -d "$project_directory" ]; then
+    echo "Directory $project_directory already exists" 
+else
+    mkdir -p "$project_directory"
+     echo "Created project directory: $project_directory"
+fi
+cd "$project_directory"
 
 # creating attendance_checker.py file
 touch  attendance_checker.py
@@ -44,31 +50,32 @@ touch reports.log
 
 #2. Dynamic Configuration (Stream Editing)
 # deciding if I want  to update the attendance thresholds
-echo "Do I want to update the attendance thresholds? (y/n)"
+echo "Do I want to update the attendance thresholds? y/n"
 read update_choice
-if [ $update_choice = y ]; then
+if [ "$update_choice" = "y" ]; then
         echo "new warning threshold (default 75):"
         read warning
         warning=${warning:-75}
         
         #checking if warning is a number only
-	if ! [[ "$warning" =~ ^[0-9]+$ ]]; then
-		echo "Invalid input. Warning must be a number. 	With default 75"		
-		warning=75
-	fi	
+        if ! [[ "$warning" =~ ^[0-9]+$ ]]; then
+                echo "Invalid input. Warning must be a number.  With default 75"                
+                warning=75
+        fi      
 
-	echo "Enter new Failure threshold (default 50):"
+        echo "Enter new Failure threshold (default 50):"
         read failure
         failure=${failure:-50}
 
-	# checking if failure is a number 
-	if ! [[ "$failure" =~ ^[0-9]+$ ]]; then
-		echo "Invalid input. failure  must be a number. With default as 50"
-		failure=50
-	fi
+        # checking if failure is a number 
+        if ! [[ "$failure" =~ ^[0-9]+$ ]]; then
+                echo "Invalid input. failure  must be a number. With default as 50"
+                failure=50
+        fi
 # using the sed command to perform an "in-place" edit of the config.json file
-sed -i "s/\"warning\": [0-9]\+/\"warning\": $warning/" /root/deploy_agent_Roheya/attendance_tracker_v1/Helpers/config.json
-sed -i "s/\"failure\": [0-9]\+/\"failure\": $failure/" /root/deploy_agent_Roheya/attendance_tracker_v1/Helpers/config.json
+# using the sed command to perform an "in-place" edit of the config.json file
+sed -i "s/\"warning\": [0-9]\+/\"warning\": $warning/" $project_directory/Helpers/config.json
+sed -i "s/\"failure\": [0-9]\+/\"failure\": $failure/" $project_directory/Helpers/config.json
 else
     echo "no changes"
 fi
@@ -82,10 +89,8 @@ else
         echo "Warning: Python3 is not installed."
 fi
 # Verify application directory structure
-project_directory="/root/deploy_agent_Roheya/attendance_tracker_v1"
 if [ -d "$project_directory" ];then
         echo "Success: Application directory exists at $project_directory"
 else
         echo "Warning: Application directory missing: $project_directory"
 fi
-
